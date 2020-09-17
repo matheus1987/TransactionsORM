07563d2576c4ca27162062c3b0e2fc9519cdb2bf
Matheus Alves Lemos <matheus@osm.com.br>
2020-09-17 07:45:46 -0300

    Migrations e versao inicial dos models

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,16 +1,30 @@
+import {Column, Entity,PrimaryGeneratedColumn,CreateDateColumn, UpdateDateColumn,ManyToOne,JoinColumn} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
-
+  
+  @Column()
   title: string;
-
+  
+  @Column()
   type: 'income' | 'outcome';
-
+  
+  @Column()
   value: number;
-
+  
+  @Column()
   category_id: string;
+  @ManyToOne(()=>Category)
+  @JoinColumn({name:'category_id'})
+  provider: Category;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
 
